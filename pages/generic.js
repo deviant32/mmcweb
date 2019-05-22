e422863efad582eb759204023092f9dd8ea9d9e1
Joe Kolba <joekolba09@gmail.com>
2019-05-22 16:24:48 -0400

    removed unused pages

--- pages/generic.js
+++ pages/generic.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import Router from 'next/router';
 
-import { mapGlobals } from 'utils/helperFuncs';
 import Head from 'next/head';
-import Meta from 'components/widgets/Meta';
-import Page from 'components/views/page'
-import Header from 'components/views/partials/header'
-import Footer from 'components/views/partials/footer'
-import Request from 'utils/request';
+import { mapGlobals } from '../utils/helperFuncs';
+import Meta from '../components/widgets/Meta';
+import Page from '../components/views/page'
+import Header from '../components/views/partials/header'
+import Footer from '../components/views/partials/footer'
+import Request from '../utils/request';
 
 class DynamicPage extends React.Component {
 
