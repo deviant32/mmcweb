167b4fe69be336939b91ea6226d5d3ca097ee05e
Joe Kolba <joekolba09@gmail.com>
2019-07-03 10:46:26 -0400

    changed sanity config

--- client.js
+++ client.js
@@ -4,5 +4,6 @@ const sanityClient = require('@sanity/client');
 module.exports = sanityClient({
   projectId: 'lg39ot3x',
   dataset: 'production',
-  useCdn: true
+  watchMode: true,
+  useCdn: false
 })
