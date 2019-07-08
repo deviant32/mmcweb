d0b87a432e92cbff9a239c087a1a0dd7f070e713
Joe Kolba <joekolba09@gmail.com>
2019-07-08 08:00:54 -0400

    reverted cdn setting

--- client.js
+++ client.js
@@ -4,6 +4,5 @@ const sanityClient = require('@sanity/client');
 module.exports = sanityClient({
   projectId: 'lg39ot3x',
   dataset: 'production',
-  watchMode: true,
-  useCdn: false
+  useCdn: true
 })
