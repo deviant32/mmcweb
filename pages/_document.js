37473f51074f7f2e8da45f62fec98454a1ef6283
Joe Kolba <joekolba09@gmail.com>
2019-07-09 13:44:54 -0400

    changed google tags to use live version

--- pages/_document.js
+++ pages/_document.js
@@ -26,9 +26,9 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/x-icon" href="/static/img/favicon.ico" />
           <script dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=NhpXQpmoQScO-4oOPyRzew&gtm_preview=env-2&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','${GOOGLE_TAGS}');`,
           }}
           />
@@ -36,7 +36,7 @@ export default class MyDocument extends Document {
         <body>
           <noscript>
             <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-MB79W62"
+              src={`https://www.googletagmanager.com/ns.html?id=${GOOGLE_TAGS}&gtm_auth=NhpXQpmoQScO-4oOPyRzew&gtm_preview=env-2&gtm_cookies_win=x`}
               height="0"
               width="0"
               style={{ display: 'none', visibility: 'hidden' }}
