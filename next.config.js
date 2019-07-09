405105452016a20339c74f0fc898be6b85aaacff
Joe Kolba <joekolba09@gmail.com>
2019-07-09 13:16:28 -0400

    added dependency and removed package lock from ignore

--- next.config.js
+++ next.config.js
@@ -6,70 +6,68 @@ const withSass = require('@zeit/next-sass');
 const sanity = require('./client');
 
 module.exports = withSass({
-    webpack: config => {
-        config.plugins = config.plugins || [];
+  webpack: config => {
+    config.plugins = config.plugins || [];
 
-        config.plugins = [
-            ...config.plugins,
+    config.plugins = [
+      ...config.plugins,
 
-            // Read the .env file
-            new Dotenv({
-                path: path.join(__dirname, '.env'),
-                systemvars: true
-            })
-        ]
+      // Read the .env file
+      new Dotenv({
+        path: path.join(__dirname, '.env'),
+        systemvars: true
+      })
+    ]
 
-        return config
-    },
-    publicRuntimeConfig: {
-        GOOGLE_TAGS: process.env.GOOGLE_TAGS
-    },
-    exportPathMap: async (defaultPathMap) => {
+    return config
+  },
+  publicRuntimeConfig: {
+    GOOGLE_TAGS: process.env.GOOGLE_TAGS
+  },
+  exportPathMap: async (defaultPathMap) => {
 
-        // fetch all of the case studies and make pages
-        const case_studies = await sanity.fetch('*[_type == "case_study" && defined(slug)].slug.current');
+    // fetch all of the case studies and make pages
+    const case_studies = await sanity.fetch('*[_type == "case_study" && defined(slug)].slug.current');
 
-        var pages = {};
+    var pages = {};
 
-        case_studies.forEach((page) => {
-            pages[`/case-studies/${page}`] = { page: '/case-studies/[title]', query: { title: page } };
-        });
+    case_studies.forEach((page) => {
+      pages[`/case-studies/${page}`] = { page: '/case-studies/[title]', query: { title: page } };
+    });
 
-        console.log(defaultPathMap);
-
-        return Object.assign({}, pages, {
-            '/': { page: '/' },
-            '/growth-reinvented': { page: '/growth-reinvented' },
-            '/resources': { page: '/resources' },
-            '/contact-us': { page: '/contact-us' },
-            '/thank-you': { page: '/thank-you' },
-            '/team': { page: '/team' },
-            '/case-studies': { page: '/case-studies' },
-            '/industries': { page: '/industries' },
-            '/capabilities': { page: '/capabilities' },
-            '/community': { page: '/community' },
-            '/industries/healthcare': { page: '/case-studies' },
-            '/careers': { page: '/team' },
-            '/contact': { page: '/contact-us' },
-            '/company': { page: '/growth-reinvented' },
-            '/new-products': { page: '/growth-reinvented' },
-        });
+    return Object.assign({}, pages, {
+      '/': { page: '/' },
+      '/growth-reinvented': { page: '/growth-reinvented' },
+      '/resources': { page: '/resources' },
+      '/contact-us': { page: '/contact-us' },
+      '/thank-you': { page: '/thank-you' },
+      '/team': { page: '/team' },
+      '/case-studies': { page: '/case-studies' },
+      '/industries': { page: '/industries' },
+      '/capabilities': { page: '/capabilities' },
+      '/community': { page: '/community' },
+      '/industries/healthcare': { page: '/case-studies' },
+      '/careers': { page: '/team' },
+      '/contact': { page: '/contact-us' },
+      '/company': { page: '/growth-reinvented' },
+      '/new-products': { page: '/growth-reinvented' },
+    });
 
 
-        /* not working yet....
-        const paths = await sanity
-            .fetch('*[_type == "case_study" && defined(slug)].slug.current')
-            .then(data =>
-                data.reduce(
-                    (acc, slug) => ({
-                        '/': { page: '/' },
-                        ...acc,
-                        [`/case-studies/${slug}`]: { page: '/case-studies/[title]', query: { title: slug } }
-                    }),
-                    defaultPathMap
-                )
+    /* not working yet....
+    const paths = await sanity
+        .fetch('*[_type == "case_study" && defined(slug)].slug.current')
+        .then(data =>
+            data.reduce(
+                (acc, slug) => ({
+                    '/': { page: '/' },
+                    ...acc,
+                    [`/case-studies/${slug}`]: { page: '/case-studies/[title]', query: { title: slug } }
+                }),
+                defaultPathMap
             )
-            .catch(console.error)
-        return paths;*/
-    }
+        )
+        .catch(console.error)
+    return paths;*/
+  }
 })
