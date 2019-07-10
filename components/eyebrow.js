37a7af44db020b7cc72dd1edc80b00ae04fd1d65
Joe Kolba <joekolba09@gmail.com>
2019-07-10 07:54:31 -0400

    formatting issues

--- components/eyebrow.js
+++ components/eyebrow.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Eyebrow = (props) => {
   const { children, className } = props;
   return (
-    <h2 className={`eyebrow text-uppercase font-weight-normal tre pt-3 ${className}`} style={{fontSize: '1.3em'}}>
+    <h2 className={`eyebrow text-uppercase font-weight-normal tre pt-3 ${className}`} style={{ fontSize: '1.3em' }}>
       {children}
     </h2>
   )
